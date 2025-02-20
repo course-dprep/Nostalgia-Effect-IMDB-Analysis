ed25cb07556f615ddb54ea31498fba829f342016
Eva Sozzi <evasozzi@air-di-eva.home>
2025-02-20 12:38:20 +0100

    Creation of the mean of the numVotes variable for further analysis.

--- clean_title_ratings.R
+++ clean_title_ratings.R
@@ -5,9 +5,6 @@ if (!"title.ratings" %in% names(datasets)) {
   stop("Dataset 'title.ratings' not found in the 'datasets' list. Make sure it was loaded correctly.")
 }
 
-
-
-
 # Assign the dataset to a variable for easier manipulation
 ratings_df <- datasets$title.ratings
 
@@ -34,3 +31,20 @@ write_tsv(ratings_df_cleaned, file.path(tempdir(), "title.ratings_cleaned.tsv"))
 
 cat("Cleaning complete. Cleaned dataset saved as 'title.ratings_cleaned.tsv'.\n")
 
+# Calculate mean of numVotes (using the cleaned dataset)
+mean_numVotes <- mean(ratings_df_cleaned$numVotes, na.rm=TRUE)
+cat("Mean of numVotes:", mean_numVotes, "\n")
+
+# Filter films with numVotes above or equal to the mean
+ratings_above_mean <- ratings_df %>% 
+  filter(numVotes >= mean_numVotes)
+
+# Visualize the number of films with rating above the mean
+cat("Number of films with numVotes over the mean:", nrow(ratings_above_mean), "\n")
+
+# Filter with numVotes below the mean
+ratings_below_mean <- ratings_df %>% 
+  filter(numVotes < mean_numVotes)
+
+# Visualize the number of films with rating below the mean 
+cat("Number of films with numVotes below the mean:", nrow(ratings_below_mean), "\n")
