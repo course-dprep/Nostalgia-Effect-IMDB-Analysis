31b8a412ad4f0732399d699f6d32e105c3efe2f9
KiminCC <m.k.capuani@tilburguniversity.edu>
2025-02-20 09:21:18 +0100

    File to work on the data cleaning of title basics (initial file), to be merged in the final code

---
+++ clean_title_basics.R
@@ -0,0 +1,23 @@
+# set up
+
+## install the deplyr package if not installed
+install.packages("dplyr")
+library(data.table) # as it is the best method for large datasets
+install.packages('R.utils')
+library(R.utils)
+library(tidyverse)
+
+
+# Read the compressed TSV file directly --> transform value which are not correct as NA.
+title_basics <- fread("https://datasets.imdbws.com/title.basics.tsv.gz", sep = "\t", na.strings ="\\N")
+colSums(is.na(title_basics)) # to check how many columns are there
+
+# Remove not needed column value --> for now just endYear (as it is just for tv series) and primary title git pu
+title_basics[, endYear :=NULL]
+View(title_basics)
+
+# Mantain just Movie and TVmovie
+unique(title_basics$titleType) #to find which type are inside
+title_basics <- title_basics %>% filter(titleType %in% c("movie", "tvMovie")) # to clean it
+sum(is.na(title_basics$startYear))
+max(title_basics$startYear, na.rm = T)
